d320b046eda2197f06937ca07fbf61fc92335cb1
lecaovan2000 <lecaovan2000@gmail.com>
2021-09-09 16:50:14 +0700

    push code bt tuan 1

---
+++ thtuan1/btTuan1.java
@@ -0,0 +1,14 @@
+package thtuan1;
+
+import java.io.*;
+public class btTuan1 {
+   public static void main(String[] args) {
+      try{
+         int ch = System.in.read();
+         System.out.println("ky tu " + (char)ch + " co ma ascii = "+ ch);
+      }
+      catch(IOException a){
+            System.out.println(a);
+      }
+   }
+}
